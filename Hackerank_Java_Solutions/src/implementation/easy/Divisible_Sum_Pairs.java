d5a40f9630a0f88e904950cd63eca9fe800467f6
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-31 22:58:40 +0530

    Divisible_Sum_Pairs

---
+++ Hackerank_Java_Solutions/src/implementation/easy/Divisible_Sum_Pairs.java
@@ -0,0 +1,31 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+Date     : 31-May-2020
+*/
+
+package implementation.easy;
+
+public class Divisible_Sum_Pairs
+{
+	static int divisibleSumPairs(int n, int k, int[] ar)
+	{
+
+		int count = 0;
+		for (int i = 0; i < ar.length - 1; i++)
+		{
+			for (int j = i + 1; j < ar.length; j++)
+			{
+				if ((ar[i] + ar[j]) % k == 0)
+				{
+					count++;
+				}
+			}
+		}
+		return k;
+
+	}
+}
+
+/* https://github.com/shahiddhariwala */
