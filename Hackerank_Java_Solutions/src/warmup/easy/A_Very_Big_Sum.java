9e22a4beec25652eeb628453db5269b244193acb
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-05-31 18:39:13 +0530

    A Very Big Sum

---
+++ Hackerank_Java_Solutions/src/warmup/easy/A_Very_Big_Sum.java
@@ -0,0 +1,23 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+Date     : 31-May-2020
+*/
+
+package warmup.easy;
+
+public class A_Very_Big_Sum
+{
+	static long aVeryBigSum(long[] ar)
+	{
+		long sum = 0;
+		for (long i : ar)
+		{
+			sum += i;
+		}
+		return sum;
+	}
+}
+
+/* https://github.com/shahiddhariwala */
