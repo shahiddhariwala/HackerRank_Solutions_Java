9c44e4b79cfdd0115f836deef996d073696c9990
shahiddhariwala <shahid.dhariwala@gmail.com>
2020-06-10 11:55:30 +0530

    Sequence Equation

---
+++ Hackerank_Java_Solutions/src/implementation/medium/Sequence_Equation.java
@@ -0,0 +1,33 @@
+/*
+Code by  : Shahid Dhariwala
+LinkedIn : https://www.linkedin.com/in/shahiddhariwala/
+Twitter  : https://twitter.com/shahiddhariwala
+Date     : 10-Jun-2020
+*/
+
+
+package implementation.medium;
+
+import java.util.HashMap;
+
+public class Sequence_Equation
+{
+    static int[] permutationEquation(int[] p) {
+		HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for(int i=0;i<p.length;i++)
+        {
+            hm.put(p[i],i+1);
+        }
+        int x[] = new int[p.length];
+        for(int i=1;i<=p.length;i++)
+        {
+            x[i-1] = hm.get(hm.get(i));
+        }
+    return x;
+    }
+}
+
+
+
+/*https://github.com/shahiddhariwala*/
